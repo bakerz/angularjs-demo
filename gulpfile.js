00422521cb0abee21d445b48923c8b5d505d521c
bakerz <de1719@163.com>
2017-02-09 18:31:32 +0800

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,29 @@
 'use strict';
 
 var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+var reload = bs.reload;
 
-gulp.task('default', function() {
-    console.log('gulp default');
+//静态服务器
+gulp.task('serve', function() {
+    bs.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('client/public/styles/scss/*.scss', ['sass']);
+    gulp.watch('*.html').on('change', reload);
+    gulp.watch('client/views/*.html').on('change', reload);
 });
+
+// scss编译后的css将注入到浏览器里实现更新
+gulp.task('sass', function() {
+    return gulp.src('client/public/styles/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('client/public/styles/css'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['serve']);
