cb8279c6aafa2000cd9dfcbc5abd7c63e4b5c08a
bakerz <de1719@163.com>
2017-03-06 17:56:07 +0800

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,18 @@ var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
 var reload = bs.reload;
 
-//静态服务器
+// 路径配置
+var path = {
+    input: {
+        js: [''],
+        scss: [''],
+    },
+    output: {
+        dist: 'dist'
+    }
+}
+
+// 静态服务器
 gulp.task('serve', function() {
     bs.init({
         server: {
@@ -15,32 +26,34 @@ gulp.task('serve', function() {
         }
     });
 
-    gulp.watch('client/public/styles/scss/*.scss', ['sass'], reload);
-    gulp.watch('*.html').on('change', reload);
-    gulp.watch('client/views/*.html').on('change', reload);
-    gulp.watch('client/public/scripts/*.js').on('change', reload);
+    gulp.watch('src/assets/styles/scss/*.scss', ['sass'], reload);
+    gulp.watch([
+        '*.html',
+        'src/app/views/*.html',
+        'src/app/scripts/*.js'
+    ]).on('change', reload);
 });
 
 // scss编译后的css将注入到浏览器里实现更新
 gulp.task('sass', function() {
-    return gulp.src('client/public/styles/scss/*.scss')
+    return gulp.src('src/assets/styles/scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('client/public/styles/css'))
+        .pipe(gulp.dest('src/assets/styles/css'))
         .pipe(reload({stream: true}));
 });
 
 // 注入 css、javascript、插件
-gulp.task('inject', function() {
+gulp.task('inject', ['sass'], function() {
     var target = gulp.src('./index.html');
 
     var headSources = gulp.src([
-        'client/public/styles/css/**/*.css',
-        'client/public/scripts/**/route.js'
+        'src/assets/styles/css/**/*.css',
+        'src/app/scripts/**/route.js'
         ], {read: false});
 
     var bodySources = gulp.src([
-        '!./client/public/scripts/route.js',
-        './client/public/scripts/**/*.js'
+        '!src/app/scripts/route.js',
+        'src/app/scripts/**/*.js'
         ], {read: false});
 
     return target.pipe(inject(headSources, {starttag: '<!-- inject:head:{{ext}} -->'}))
