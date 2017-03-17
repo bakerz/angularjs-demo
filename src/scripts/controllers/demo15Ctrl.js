91fd5b466e868782fc6ee89327a39d5eed670d00
bakerz <de1719@163.com>
2017-03-17 16:20:19 +0800

    demo15

--- src/scripts/controllers/demo15Ctrl.js
+++ src/scripts/controllers/demo15Ctrl.js
@@ -1,79 +1,133 @@
 'use strict';
 
 angular.module('myApp')
-.directive('targetItem', function() {
-  return {
-    restrict: 'A',
-    replace: true,
-    template: '<div class="destination-area"'+
-    '> + </div>',
-    link: function(scope, elem, attrs) {
-      var dragenter = function(event) {
-        console.log('dir-enter');
-        event.target.style.border = '2px dashed #000';
-      };
+  .directive('simDragItem', function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'sim-drag-item.html',
+      link: function (scope, elem, attr) {
+        var dragStart = function (event) {
+          console.log('dir-start');
+          event.dataTransfer.setData("Text",event.target.id);
+          document.getElementById("demo").innerHTML = "开始拖动 p 元素";
+        };
 
-      var dragleave = function(event) {
-        console.log('dir-leave');
-        event.target.style.border = '1px dashed #ccc';
-      };
+        var dragEnd = function(event) {
+          document.getElementById("demo").innerHTML = "完成 p 元素的拖动";
+        };
 
-      var dragover = function(event) {
-        console.log('dir-over');
-        event.preventDefault();
-      };
-
-      var drop = function(drop) {
-        event.target.style.border = 'none';
-        event.target.style.lineHeight = '0px';
-        event.target.style.height = 'auto';
-        event.target.style.width = 'auto';
-        event.target.style.textAlign = 'left';
-        event.target.innerHTML = '<div class="des-area-item" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>'+
-          '<div class="input-item">'+scope.elemNameSuffix+'</div>'+
-          '<div class="des-area-indent" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>'+
-          '<div class="input-item">'+scope.elemNamePrefix+'</div>'+
-          '<div class="des-area-item" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>';
-        event.target.ondragenter = null;
-        event.target.ondragover = null;
-        event.target.ondragleave = null;
-        event.target.ondrop = null;
-      };
-      elem.bind('dragenter', dragenter)
-        .bind('dragleave', dragleave)
-        .bind('dragover', dragover)
-        .bind('drop', drop)
+        elem.bind('dragstart', dragStart)
+          .bind('dragend', dragEnd)
+      }
     }
-  }
-})
-.directive('dragItem', function() {
-  return {
-    restrict: 'A',
-    replace: true,
-    template: '<div class="drag-item" draggable="true">'+
-    '<div> + </div>'+
-    '<input type="text" placeholder="<?>" class="input-item" ng-model="elemNamePrefix">'+
-    '<div> + </div>'+
-    '<input type="text" placeholder="</?>" class="input-item" ng-model="elemNameSuffix" disabled="disabled">'+
-    '<div> + </div>'+
-    '</div>',
-    link: function(scope, elem, attrs) {
-      var dragstart = function(event) {
-        console.log('dir-start');
-      };
+  })
+  .directive('simTargetItem', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'sim-target-item.html',
+      link: function (scope, elem, attr) {
+        var dragEnter = function(event) {
+          console.log('dir-enter');
+        };
 
-      var dragend = function(event) {
-        console.log('dir-end');
-      };
+        var dragLeave = function(event) {
+          console.log('dir-leave');
+        };
 
-      elem.bind('dragstart', dragstart)
-        .bind('dragend', dragend);
-      scope.$watch('elemNamePrefix', function(newVal, oldVal, scope) {
-        if (newVal != oldVal) {
-          console.log(333);
-          scope.elemNameSuffix = newVal.replace('<','</');
-        }
-      })
+        var dragOver = function (event) {
+          console.log('dir-over');
+          event.preventDefault();
+        };
+
+        var drop = function(event) {
+          console.log('dir-drop');
+          var data=event.dataTransfer.getData("Text");
+          event.target.appendChild(document.getElementById(data));
+          event.preventDefault();
+        };
+
+        elem.bind('dragover', dragOver)
+          .bind('dragenter', dragEnter)
+          .bind('dragleave', dragLeave)
+          .bind('drop', drop);
+      }
     }
-  }
-});
+  })
+  .directive('targetItem', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      template: '<div class="destination-area"'+
+      '> + </div>',
+      link: function(scope, elem, attrs) {
+        var dragenter = function(event) {
+          console.log('dir-enter');
+          event.target.style.border = '2px dashed #000';
+        };
+  
+        var dragleave = function(event) {
+          console.log('dir-leave');
+          event.target.style.border = '1px dashed #ccc';
+        };
+  
+        var dragover = function(event) {
+          console.log('dir-over');
+          event.preventDefault();
+        };
+  
+        var drop = function(drop) {
+          event.target.style.border = 'none';
+          event.target.style.lineHeight = '0px';
+          event.target.style.height = 'auto';
+          event.target.style.width = 'auto';
+          event.target.style.textAlign = 'left';
+          event.target.innerHTML = '<div class="des-area-item" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>'+
+            '<div class="input-item">'+scope.elemNameSuffix+'</div>'+
+            '<div class="des-area-indent" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>'+
+            '<div class="input-item">'+scope.elemNamePrefix+'</div>'+
+            '<div class="des-area-item" ondragenter="dragenter(event)" ondragover="dragover(event)" ondragleave="dragleave(event)" ondrop="drop(event)"> + </div>';
+          event.target.ondragenter = null;
+          event.target.ondragover = null;
+          event.target.ondragleave = null;
+          event.target.ondrop = null;
+        };
+        elem.bind('dragenter', dragenter)
+          .bind('dragleave', dragleave)
+          .bind('dragover', dragover)
+          .bind('drop', drop)
+      }
+    }
+  })
+  .directive('dragItem15', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      template: '<div class="drag-item" draggable="true">'+
+      '<div> + </div>'+
+      '<input type="text" placeholder="<?>" class="input-item" ng-model="elemNamePrefix">'+
+      '<div> + </div>'+
+      '<input type="text" placeholder="</?>" class="input-item" ng-model="elemNameSuffix">'+
+      '<div> + </div>'+
+      '</div>',
+      link: function(scope, elem, attrs) {
+        var dragstart = function(event) {
+          console.log('dir-start');
+        };
+  
+        var dragend = function(event) {
+          console.log('dir-end');
+        };
+  
+        elem.bind('dragstart', dragstart)
+          .bind('dragend', dragend);
+        scope.$watch('elemNamePrefix', function(newVal, oldVal, scope) {
+          if (newVal != oldVal) {
+            console.log(333);
+            scope.elemNameSuffix = newVal.replace('<','</');
+          }
+        })
+      }
+    }
+  });
--- src/scripts/route.js
+++ src/scripts/route.js
@@ -1,157 +1,157 @@
 'use strict';
 
 angular.module('app-router', [])
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider
-    .when('', '/home')
-    .otherwise('home');
+      .when('', '/home')
+      .otherwise('home');
 
     $stateProvider
-    .state('home', {
+      .state('home', {
         url: '/home',
         views: {
-            'header': {
-                templateUrl: 'views/uiHeader.html'
-            },
-            'nav': {
-                templateUrl: 'views/uiNav.html'
-            },
-            'content': {
-                templateUrl: 'views/uiContentDefault.html'
-            }
+          'header': {
+            templateUrl: 'views/uiHeader.html'
+          },
+          'nav': {
+            templateUrl: 'views/uiNav.html'
+          },
+          'content': {
+            templateUrl: 'views/uiContentDefault.html'
+          }
         }
-    })
-    .state('home.demo01', {
+      })
+      .state('home.demo01', {
         url:'/demo01',
         views: {
-            'content@': {
-                templateUrl: 'views/demo01.html'
-            }
+          'content@': {
+            templateUrl: 'views/demo01.html'
+          }
         }
-    })
-    .state('home.demo02', {
+      })
+      .state('home.demo02', {
         url:'/demo02',
         views: {
-            'content@': {
-                templateUrl: 'views/demo02.html',
-                controller: 'demo02Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo02.html',
+            controller: 'demo02Ctrl'
+          }
         }
-    })
-    .state('home.demo03', {
+      })
+      .state('home.demo03', {
         url:'/demo03',
         views: {
-            'content@': {
-                templateUrl: 'views/demo03.html',
-                controller: 'demo03Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo03.html',
+            controller: 'demo03Ctrl'
+          }
         }
-    })
-    .state('home.demo04', {
+      })
+      .state('home.demo04', {
         url:'/demo04',
         views: {
-            'content@': {
-                templateUrl: 'views/demo04.html',
-                controller: 'demo04Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo04.html',
+            controller: 'demo04Ctrl'
+          }
         }
-    })
-    .state('home.demo05', {
+      })
+      .state('home.demo05', {
         url:'/demo05',
         views: {
-            'content@': {
-                templateUrl: 'views/demo05.html',
-                controller: 'demo05Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo05.html',
+            controller: 'demo05Ctrl'
+          }
         }
-    })
-    .state('home.demo06', {
+      })
+      .state('home.demo06', {
         url:'/demo06',
         views: {
-            'content@': {
-                templateUrl: 'views/demo06.html'
-            }
+          'content@': {
+            templateUrl: 'views/demo06.html'
+          }
         }
-    })
-    .state('home.demo07', {
+      })
+      .state('home.demo07', {
         url:'/demo07',
         views: {
-            'content@': {
-                templateUrl: 'views/demo07.html',
-                controller: 'demo07Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo07.html',
+            controller: 'demo07Ctrl'
+          }
         }
-    })
-    .state('home.demo08', {
+      })
+      .state('home.demo08', {
         url:'/demo08',
         views: {
-            'content@': {
-                templateUrl: 'views/demo08.html',
-                controller: 'demo08Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo08.html',
+            controller: 'demo08Ctrl'
+          }
         }
-    })
-    .state('home.demo09', {
+      })
+      .state('home.demo09', {
         url:'/demo09',
         views: {
-            'content@': {
-                templateUrl: 'views/demo09.html',
-                controller: 'demo09Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo09.html',
+            controller: 'demo09Ctrl'
+          }
         }
-    })
-    .state('home.demo10', {
+      })
+      .state('home.demo10', {
         url:'/demo10',
         views: {
-            'content@': {
-                templateUrl: 'views/demo10.html',
-                controller: 'demo10Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo10.html',
+            controller: 'demo10Ctrl'
+          }
         }
-    })
-    .state('home.demo11', {
+      })
+      .state('home.demo11', {
         url:'/demo11',
         views: {
-            'content@': {
-                templateUrl: 'views/demo11.html',
-                controller: 'demo11Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo11.html',
+            controller: 'demo11Ctrl'
+          }
         }
-    })
-    .state('home.demo12', {
+      })
+      .state('home.demo12', {
         url:'/demo12',
         views: {
-            'content@': {
-                templateUrl: 'views/demo12.html',
-                controller: 'demo12Ctrl'
-            }
+          'content@': {
+            templateUrl: 'views/demo12.html',
+            controller: 'demo12Ctrl'
+          }
         }
-    })
-    .state('home.demo13', {
+      })
+      .state('home.demo13', {
         url:'/demo13',
         views: {
-            'content@': {
-                templateUrl: 'views/demo13.html',
-                controller: 'demo13Ctrl'
-            }
-        }
-    })
-      .state('home.demo14', {
-          url:'/demo14',
-          views: {
-              'content@': {
-                  templateUrl: 'views/demo14.html',
-                  controller: 'demo14Ctrl'
-              }
+          'content@': {
+            templateUrl: 'views/demo13.html',
+            controller: 'demo13Ctrl'
           }
+        }
       })
-    .state('home.demo15', {
+      .state('home.demo14', {
+        url:'/demo14',
+        views: {
+          'content@': {
+            templateUrl: 'views/demo14.html',
+            controller: 'demo14Ctrl'
+          }
+        }
+      })
+      .state('home.demo15', {
         url:'/demo15',
         views: {
-            'content@': {
-                templateUrl: 'views/demo15.html'
-            }
+          'content@': {
+            templateUrl: 'views/demo15.html'
+          }
         }
-    });
-});
+      });
+  });
