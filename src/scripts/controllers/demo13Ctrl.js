c2e32d8f53d6dd563f196b9abc8ec66ea15cef2f
bakerz <de1719@163.com>
2017-04-15 13:10:44 +0800

    demo10 & 13

--- src/scripts/controllers/demo13Ctrl.js
+++ src/scripts/controllers/demo13Ctrl.js
@@ -4,20 +4,20 @@ angular.module('myApp')
   .controller('demo13Ctrl', function ($scope) {
     $scope.arr = [];
     $scope.addData = function() {
-      $scope.arr.push('xxxxx');
+      $scope.arr.push(Date.now());
       $scope.refresh = Date.now();
     }
   })
   .directive('dirTest', function() {
     return {
       restrict: 'EA',
-      template: '<div class="test">{{data}}</div>',
+      template: '<div class="test" ng-repeat="data in arr">{{data | date: "yyyy-MM-dd HH:mm:ss"}}</div>',
       link: function(scope, element, attrs) {
         scope.$watch(attrs.refreshData, function(newValue) {
-          if (newValue) {
+          /*if (newValue) {
             console.log(attrs.dirData);
             scope.data = attrs.dirData;
-          }
+          }*/
         })
       }
     }
