5d142d425e8ccd3087351b47c9fa463747ebdf99
bakerz <de1719@163.com>
2017-04-25 16:10:21 +0800

    'demo17'

--- src/scripts/directives/hidePanelDir.js
+++ src/scripts/directives/hidePanelDir.js
@@ -1,32 +1,18 @@
 'use strict';
 
 angular.module('myApp')
-  .controller('demo17Ctrl', function ($scope) {
+  .controller('demo17Ctrl', function ($scope, $document) {
     $scope.conShow = false;
-    $scope.sum = 0;
-  })
-  .directive('hidePlane', function () {
-    return {
-      restrict: 'A',
-      scope: false,
-      link: function (scope, elem, attr) {
-        console.log('==========', elem, attr);
 
-        elem.on('click', function () {
-          scope.sum++;
-        });
+    $scope.showListClick=function($event, isBtn){
+      $event.stopPropagation();
+      //$scope.conShow=true;
 
+      $scope.conShow = $scope.conShow && isBtn ? false : true;
+    };
 
-        document.addEventListener('click', function (e) {
-          console.log('target:', e, e.target, e.target.id);
-          if (e.path[1].id != 'hide-panel' && e.path[2].id != 'hide-panel') {
-            console.log('关闭');
-            scope.conShow = false;
-            console.log(scope.conShow);
-          } else {
-            console.log('======', e.target.id);
-          }
-        })
-      }
-    }
+    $document.bind('click', function () {
+      $scope.conShow = false;
+      $scope.$apply();
+    })
   });
