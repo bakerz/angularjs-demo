c060865256268050d14d91d7e93bf47a7b2c8e04
bakerz <de1719@163.com>
2017-07-06 15:10:49 +0800

    update nav

---
+++ src/scripts/controllers/navCtrl.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('myApp').controller('navCtrl',
+  function ($scope) {
+    $scope.navs = [{
+      name: 'demo01',
+      link: 'demo01'
+    },{
+      name: 'demo02',
+      link: 'demo02'
+    },{
+      name: 'demo03',
+      link: 'demo03'
+    },{
+      name: 'demo04',
+      link: 'demo04'
+    },{
+      name: 'demo05',
+      link: 'demo05'
+    },{
+      name: 'demo06',
+      link: 'demo06'
+    },{
+      name: 'demo07',
+      link: 'demo07'
+    },{
+      name: 'demo08',
+      link: 'demo08'
+    },{
+      name: 'demo09',
+      link: 'demo09'
+    },{
+      name: 'demo10',
+      link: 'demo10'
+    },{
+      name: 'demo11',
+      link: 'demo11'
+    },{
+      name: 'demo12',
+      link: 'demo12'
+    },{
+      name: 'demo13',
+      link: 'demo13'
+    },{
+      name: 'demo14',
+      link: 'demo14'
+    },{
+      name: 'demo15',
+      link: 'demo15'
+    },{
+      name: 'demo16',
+      link: 'demo16'
+    },{
+      name: 'demo17',
+      link: 'demo17'
+    },{
+      name: 'demo18',
+      link: 'demo18'
+    },{
+      name: 'demo19',
+      link: 'demo19.public.application'
+    }]
+  }
+);
