9a1cf8092161c72e2200d7b624fccc8405181dcb
bakerz <de1719@163.com>
2017-08-22 16:29:47 +0800

    update demo02

--- src/scripts/controllers/demo02Ctrl.js
+++ src/scripts/controllers/demo02Ctrl.js
@@ -2,17 +2,19 @@
 
 angular.module('myApp')
   .controller('demo02Ctrl', function($scope) {
-    $scope.firstName = 'John';
-    $scope.lastName = 'Doe';
+    $scope.datas = [{
+      name: 'test',
+      address: 'http://www.xxx.com'
+    }];
 
-    $scope.type = ['类别1','类别2','类别3','类别4'];
-
-    $scope.disabled = true;
-    $scope.change = function (ss) {
-      $scope.disabled = false;
+    $scope.add = function () {
+      $scope.datas.push({
+        name: '',
+        address: ''
+      })
     };
 
-    $scope.log = function () {
-      console.log($scope.ss);
+    $scope.delete = function (index) {
+      $scope.datas.splice(index, 1);
     }
   });
