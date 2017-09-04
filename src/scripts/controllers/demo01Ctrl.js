bb348f9f8a071c89c5f02ea1ffd586b8db495dc7
bakerz <de1719@163.com>
2017-09-04 19:10:35 +0800

    uodate demo01

--- src/scripts/controllers/demo01Ctrl.js
+++ src/scripts/controllers/demo01Ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp').controller('demo01Ctrl',
-  function ($scope) {
+  function ($scope,$interval) {
     $scope.colors = [{name:'black', shade:'dark'},
       {name:'white', shade:'light'},
       {name:'red', shade:'dark'},
@@ -13,6 +13,50 @@ angular.module('myApp').controller('demo01Ctrl',
       light: "red",
       lai: "red"
     };
+
+    $scope.options = [{
+      name: 'option1'
+    },{
+      name: 'option2'
+    },{
+      name: 'option3'
+    },{
+      name: 'option4'
+    }];
+
+    $scope.time = 3;
+    $interval(function () {
+      if ($scope.time > 1) {
+        $scope.time--;
+      } else {
+        $scope.time = 3;
+        if ($scope.options.length==5) {
+          //$scope.options.pop();
+          $scope.options = [{
+            name: 'option1'
+          },{
+            name: 'option2'
+          },{
+            name: 'option3'
+          },{
+            name: 'option4'
+          }];
+        } else {
+          //$scope.options.push({name: 'option5'});
+          $scope.options = [{
+            name: 'option1'
+          },{
+            name: 'option2'
+          },{
+            name: 'option3'
+          },{
+            name: 'option4'
+          },{
+            name: 'option5'
+          }];
+        }
+      }
+    },3000);
 
     $scope.selectedColor3 = $scope.colors[$scope.colors.length-1].name;
   }
